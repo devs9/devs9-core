29d3b251b7b4b32a390c7fca9a4b1f927da9e9b6
George <PolAdaShvili>
2020-08-22 08:00:12 +0300

    ⭐ Init project ⭐
    Signed-off-by: George <PolAdaShvili>

--- src/components/core/Input/Input.tsx
+++ src/components/core/Input/Input.tsx
@@ -6,6 +6,12 @@ import {InputMask} from './elements'
 import {IInput} from './ts'
 
 export {IInput}
-export const Input: FC<IInput> = memo(({mask, inputProps}) => (
-  <FormControl>{!mask ? <InputMui inputProps={inputProps} /> : <InputMask />}</FormControl>
+export const Input: FC<IInput> = memo(({mask, ...restInputProps}) => (
+  <FormControl>
+    {!mask ? (
+      <InputMui {...restInputProps} inputProps={restInputProps.inputProps} />
+    ) : (
+      <InputMask {...restInputProps} />
+    )}
+  </FormControl>
 ))
